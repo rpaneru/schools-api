7463c1007a625245f539c41dcaf91661808fead4
rajesh paneru <rpaneru1986@gmail.com>
2016-05-17 22:55:21 +0530

    oauth2 implement in api # Please enter the commit message for your changes. Lines starting

---
+++ config/autoload/global.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+    'db' => array(
+        'adapters' => array(
+            'zf2AppDbAdapter' => array(),
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'oauth' => array(
+                'options' => array(
+                    'spec' => '%oauth%',
+                    'regex' => '(?P<oauth>(/oauth))',
+                ),
+                'type' => 'regex',
+            ),
+        ),
+    ),
+);
